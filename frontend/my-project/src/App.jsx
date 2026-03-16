0f81d877c6a216266e0145bdf937c9589e59ef82
Nipesh <nipeshincbca2021@gmail.com>
2026-03-16 13:15:10 +0545

    add student component commit

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Index from './pages/Index'
+import AddStudent from './pages/AddStudent'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Index/>}  />
+      <Route path='/add' element={<AddStudent/>} />
     </Routes>
     </BrowserRouter>
   )
--- frontend/my-project/src/pages/Index.jsx
+++ frontend/my-project/src/pages/Index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import AddStudent from './AddStudent'
 
 const Index = () => {
   return (
     <>
     <div>This is index page</div>
     <p>HEllo world</p>
+    
+ <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind v3 Working 🚀
+      </h1>
+      <Link to={"/add"}>Add Student</Link>
+    </div>
+
+
     </>
  
   )
