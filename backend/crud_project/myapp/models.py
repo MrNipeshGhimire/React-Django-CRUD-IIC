16c290a88a77592b2d4c8404a514dd12f6cfb0ef
Nipesh <nipeshincbca2021@gmail.com>
2026-03-17 13:11:50 +0545

    creating model

--- backend/crud_project/myapp/models.py
+++ backend/crud_project/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Student model 
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField(unique=True)
+    faculty = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
