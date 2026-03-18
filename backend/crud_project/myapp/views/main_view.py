30e764f8a3a33716254d7da49b26b18d2e85a665
Nipesh <nipeshincbca2021@gmail.com>
2026-03-18 12:10:32 +0545

    put and delete operation commit

--- backend/crud_project/myapp/views/main_view.py
+++ backend/crud_project/myapp/views/main_view.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from ..models import Student
 from ..serializers import StudentSerializer
 
-
 # POST ,GET
 @api_view(['POST','GET'])
 def student_view(request):
@@ -25,3 +24,26 @@ def student_view(request):
         serializer =  StudentSerializer(student, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+
+@api_view(['PUT','DELETE','GET'])
+def student_view_detail(request, id):
+    student =  Student.objects.get(id=id)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'DELETE':
+        student.delete()
+        return Response({"msg":"Student data deleted successfully !!"})
+    
+    if request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Student data updated"})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
